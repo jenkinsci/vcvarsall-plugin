04c65ceb6340d3cc9392ce800e8edccbaff48219
Nathan Osman <nathan@quickmediasolutions.com>
2017-04-10 16:45:46 -0700

    Complete initial implementation of node property.

--- src/main/java/org/jenkinsci/plugins/vcvarsall/MsvcNodeProperty.java
+++ src/main/java/org/jenkinsci/plugins/vcvarsall/MsvcNodeProperty.java
@@ -4,18 +4,13 @@ import hudson.Extension;
 import hudson.model.Node;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
+import hudson.util.ListBoxModel;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * Property for configuring Visual Studio support
  */
 public class MsvcNodeProperty extends NodeProperty<Node> {
-
-    public static final String MSVC2013 = "msvc2013";
-    public static final String MSVC2015 = "msvc2015";
-
-    public static final String X86 = "x86";
-    public static final String X86_64 = "x86_64";
 
     private String mVersion;
     private String mArch;
@@ -26,20 +21,37 @@ public class MsvcNodeProperty extends NodeProperty<Node> {
         mArch = arch;
     }
 
-    String getVersion() {
+    @SuppressWarnings("WeakerAccess")
+    public String getVersion() {
         return mVersion;
     }
 
-    String getArch() {
+    @SuppressWarnings("WeakerAccess")
+    public String getArch() {
         return mArch;
     }
 
     @Extension
+    @SuppressWarnings("unused")
     public static class DescriptorImpl extends NodePropertyDescriptor {
 
         @Override
         public String getDisplayName() {
             return "Run vcvarsall.bat on startup";
+        }
+
+        public ListBoxModel doFillVersionItems() {
+            ListBoxModel items = new ListBoxModel();
+            items.add("Visual Studio 2013", "vs2013");
+            items.add("Visual Studio 2015", "vs2015");
+            return items;
+        }
+
+        public ListBoxModel doFillArchItems() {
+            ListBoxModel items = new ListBoxModel();
+            items.add("x86", "x86");
+            items.add("x86_64", "x86_64");
+            return items;
         }
     }
 }
