c4e559f002345012f44972711c66eeae8ff491ef
Nathan Osman <nathan@quickmediasolutions.com>
2017-04-11 21:22:44 -0700

    Add code for detecting vcvarsall environment variables and injecting it.

--- src/main/java/org/jenkinsci/plugins/vcvarsall/ComputerListenerImpl.java
+++ src/main/java/org/jenkinsci/plugins/vcvarsall/ComputerListenerImpl.java
@@ -1,5 +1,6 @@
 package org.jenkinsci.plugins.vcvarsall;
 
+import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.model.Computer;
@@ -8,18 +9,22 @@ import hudson.remoting.Channel;
 import hudson.slaves.ComputerListener;
 import jenkins.security.MasterToSlaveCallable;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Logger;
 
 /**
  * Listens for computers coming online and if they are configured to provide
  * MSVC compilers, runs the appropriate vcvarsall script on them.
  */
 @Extension
+@SuppressWarnings("unused")
 public class ComputerListenerImpl extends ComputerListener {
 
     /**
-     * Run vcvarsall on the agent
+     * Run vcvarsall with the correct parameters
      */
     private static class RunVcvarsallCallable
             extends MasterToSlaveCallable<Object, RuntimeException> {
@@ -34,22 +39,90 @@ public class ComputerListenerImpl extends ComputerListener {
 
         @Override
         public Object call() throws RuntimeException {
+
+            // Ensure the system is running Windows
             if (!System.getProperty("os.name").startsWith("Windows")) {
                 throw new RuntimeException("host is not running Windows");
             }
-            String installKey = System.getenv("ProgramFiles(x86)") == null ?
+
+            // Determine the actual architecture of the host
+            boolean isHost32bit = System.getenv("ProgramFiles(x86)") == null;
+
+            // Retrieve VS install directory
+            String installKey = isHost32bit ?
                     "SOFTWARE\\Microsoft\\VisualStudio\\" + mVersion :
                     "SOFTWARE\\Wow6432Node\\Microsoft\\VisualStudio\\" + mVersion;
-
+            String installDir;
             try {
-                return WinRegistry.readString(
+                installDir = WinRegistry.readString(
                         WinRegistry.HKEY_LOCAL_MACHINE,
                         installKey,
-                        "InstallRoot"
+                        "InstallDir"
                 );
             } catch (IllegalAccessException|InvocationTargetException e) {
-                return "unknown";
+                throw new RuntimeException(e.getMessage());
             }
+            if (installDir == null) {
+                throw new RuntimeException("missing InstallDir registry key");
+            }
+            installDir += "\\..\\..\\VC\\vcvarsall.bat";
+
+            // Determine the correct parameter to pass to vcvarsall
+            String param = null;
+            if (isHost32bit) {
+                switch (mArch) {
+                    case MsvcNodeProperty.X86:
+                        param = "x86";
+                        break;
+                    case MsvcNodeProperty.X86_64:
+                        param = "x86_amd64";
+                        break;
+                }
+            } else {
+                switch (mArch) {
+                    case MsvcNodeProperty.X86:
+                        param = "amd64_x86";
+                        break;
+                    case MsvcNodeProperty.X86_64:
+                        param = "amd64";
+                        break;
+                }
+            }
+            if (param == null) {
+                throw new RuntimeException("invalid arch parameter");
+            }
+
+            // Here's where things get clever - since the environment variables
+            // will disappear as soon as cmd terminates, we print them all out
+            // so that they can be used later when building
+            EnvVars envVars = new EnvVars();
+            try {
+                Process process = new ProcessBuilder(
+                        "cmd",
+                        "/c",
+                        "call",
+                        installDir,
+                        param,
+                        "&&",
+                        "set"
+                ).start();
+                BufferedReader reader=new BufferedReader(
+                        new InputStreamReader(process.getInputStream())
+                );
+                String s;
+                while ((s = reader.readLine()) != null) {
+                    s = s.trim();
+                    if (s.length() != 0) {
+                        envVars.addLine(s);
+                    }
+                }
+                process.waitFor();  // should have terminated by now
+            } catch (IOException|InterruptedException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+
+            // Return the environment variables
+            return envVars;
         }
     }
 
@@ -57,18 +130,36 @@ public class ComputerListenerImpl extends ComputerListener {
     public void preOnline(Computer c, Channel channel, FilePath root, TaskListener listener) throws IOException, InterruptedException {
         super.preOnline(c, channel, root, listener);
 
+        // Ignore any nodes that don't have the property
         MsvcNodeProperty msvcNodeProperty = c.getNode().getNodeProperties().get(MsvcNodeProperty.class);
-        if (msvcNodeProperty != null) {
-            listener.getLogger().printf(
-                    "running vcvarsall for %s (%s)\n",
-                    msvcNodeProperty.getVersion(),
-                    msvcNodeProperty.getArch()
-            );
-            Object v = channel.call(new RunVcvarsallCallable(
+        if (msvcNodeProperty == null) {
+            return;
+        }
+
+        listener.getLogger().printf(
+                "preparing to run vcvarsall for VS %s (%s)\n",
+                msvcNodeProperty.getVersion(),
+                msvcNodeProperty.getArch()
+        );
+
+        // Retrieve all of the environment variables from vcvarsall
+        EnvVars envVars;
+        try {
+            envVars = (EnvVars) channel.call(new RunVcvarsallCallable(
                     msvcNodeProperty.getVersion(),
                     msvcNodeProperty.getArch()
             ));
-            listener.getLogger().printf("value: %s\n", (String) v);
+        } catch (RuntimeException e) {
+            listener.getLogger().println(e.getMessage());
+            return;
         }
+
+        // Pass the variables to the node
+        msvcNodeProperty.setEnvVars(envVars);
+
+        listener.getLogger().printf(
+                "received %d env. variables from vcvarsall\n",
+                envVars.size()
+        );
     }
 }
--- src/main/java/org/jenkinsci/plugins/vcvarsall/MsvcNodeProperty.java
+++ src/main/java/org/jenkinsci/plugins/vcvarsall/MsvcNodeProperty.java
@@ -1,24 +1,37 @@
 package org.jenkinsci.plugins.vcvarsall;
 
+import hudson.EnvVars;
 import hudson.Extension;
-import hudson.model.Node;
+import hudson.Launcher;
+import hudson.model.*;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
 import hudson.util.ListBoxModel;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
 
 /**
  * Property for configuring Visual Studio support
  */
 public class MsvcNodeProperty extends NodeProperty<Node> {
 
+    static final String X86 = "x86";
+    static final String X86_64 = "x86_64";
+
     private String mVersion;
     private String mArch;
+    private EnvVars mEnvVars;
 
     @DataBoundConstructor
     public MsvcNodeProperty(String version, String arch) {
         mVersion = version;
         mArch = arch;
+    }
+
+    void setEnvVars(EnvVars envVars) {
+        mEnvVars = envVars;
     }
 
     @SuppressWarnings("WeakerAccess")
@@ -29,6 +42,16 @@ public class MsvcNodeProperty extends NodeProperty<Node> {
     @SuppressWarnings("WeakerAccess")
     public String getArch() {
         return mArch;
+    }
+
+    @Override
+    public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+        return Environment.create(mEnvVars);
+    }
+
+    @Override
+    public void buildEnvVars(@Nonnull EnvVars env, @Nonnull TaskListener listener) {
+        env.putAll(mEnvVars);
     }
 
     @Extension
@@ -42,15 +65,15 @@ public class MsvcNodeProperty extends NodeProperty<Node> {
 
         public ListBoxModel doFillVersionItems() {
             ListBoxModel items = new ListBoxModel();
-            items.add("Visual Studio 2013", "13.0");
+            items.add("Visual Studio 2013", "12.0");
             items.add("Visual Studio 2015", "14.0");
             return items;
         }
 
         public ListBoxModel doFillArchItems() {
             ListBoxModel items = new ListBoxModel();
-            items.add("x86", "x86");
-            items.add("x86_64", "x86_64");
+            items.add("x86", X86);
+            items.add("x86_64", X86_64);
             return items;
         }
     }
